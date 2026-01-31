925446e8093de582a2b9d801088704953a281235
larleevee <lanavalentine1889@gmail.com>
2026-01-31 18:27:07 +0000

    grrrr

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -4,17 +4,17 @@ using UnityEngine.UI;
 public class GameControl : MonoBehaviour {
 
     private static GameObject whoWinsTextShadow, player1MoveText, player2MoveText;
-
     private static GameObject player1, player2;
+
+    // Cache the scripts here
+    private static pathFollow p1Script, p2Script;
 
     public static int diceSideThrown = 0;
     public static int player1StartWaypoint = 0;
     public static int player2StartWaypoint = 0;
-
     public static bool gameOver = false;
 
     void Start () {
-
         whoWinsTextShadow = GameObject.Find("whoWinsText");
         player1MoveText = GameObject.Find("player1MoveText");
         player2MoveText = GameObject.Find("player2MoveText");
@@ -22,8 +22,12 @@ public class GameControl : MonoBehaviour {
         player1 = GameObject.Find("player1");
         player2 = GameObject.Find("player2");
 
-        player1.GetComponent<pathFollow>().moveAllowed = false;
-        player2.GetComponent<pathFollow>().moveAllowed = false;
+        // Cache the components once at the start
+        if (player1 != null) p1Script = player1.GetComponent<pathFollow>();
+        if (player2 != null) p2Script = player2.GetComponent<pathFollow>();
+
+        if (p1Script != null) p1Script.moveAllowed = false;
+        if (p2Script != null) p2Script.moveAllowed = false;
 
         whoWinsTextShadow.gameObject.SetActive(false);
         player1MoveText.gameObject.SetActive(true);
@@ -32,40 +36,41 @@ public class GameControl : MonoBehaviour {
 
     void FixedUpdate()
     {
-        if (gameOver) return;
-        
-        // Player 1 Logic
-        if (player1.GetComponent<pathFollow>().waypointIndex > 
-            player1StartWaypoint + diceSideThrown)
+        // SAFETY CHECK: If the scripts are missing, stop running to avoid the error
+        if (p1Script == null || p2Script == null) return;
+
+        // Player 1 logic
+        if (p1Script.waypointIndex > player1StartWaypoint + diceSideThrown)
         {
-            player1.GetComponent<pathFollow>().moveAllowed = false;
+            p1Script.moveAllowed = false;
             player1MoveText.gameObject.SetActive(false);
             player2MoveText.gameObject.SetActive(true);
-            player1StartWaypoint = player1.GetComponent<pathFollow>().waypointIndex - 1;
+            player1StartWaypoint = p1Script.waypointIndex - 1;
         }
 
-        // Player 2 Logic
-        if (player2.GetComponent<pathFollow>().waypointIndex > 
-            player2StartWaypoint + diceSideThrown)
+        // Player 2 logic
+        if (p2Script.waypointIndex > player2StartWaypoint + diceSideThrown)
         {
-            player2.GetComponent<pathFollow>().moveAllowed = false;
+            p2Script.moveAllowed = false;
             player2MoveText.gameObject.SetActive(false);
             player1MoveText.gameObject.SetActive(true);
-            player2StartWaypoint = player2.GetComponent<pathFollow>().waypointIndex - 1;
+            player2StartWaypoint = p2Script.waypointIndex - 1;
         }
 
-        // Win Condition Player 1
-        if (player1.GetComponent<pathFollow>().waypointIndex == 
-            player1.GetComponent<pathFollow>().waypoints.Length)
+        // Win Condition Checks
+        CheckWinConditions();
+    }
+
+    void CheckWinConditions()
+    {
+        if (p1Script.waypointIndex == p1Script.waypoints.Length)
         {
             whoWinsTextShadow.gameObject.SetActive(true);
             whoWinsTextShadow.GetComponent<Text>().text = "Player 1 Wins";
             gameOver = true;
         }
 
-        // Win Condition Player 2
-        if (player2.GetComponent<pathFollow>().waypointIndex == 
-            player2.GetComponent<pathFollow>().waypoints.Length)
+        if (p2Script.waypointIndex == p2Script.waypoints.Length)
         {
             whoWinsTextShadow.gameObject.SetActive(true);
             player1MoveText.gameObject.SetActive(false);
@@ -77,13 +82,14 @@ public class GameControl : MonoBehaviour {
 
     public static void MovePlayer(int playerToMove)
     {
+        if (gameOver) return;
+
         switch (playerToMove) {
             case 1:
-                player1.GetComponent<pathFollow>().moveAllowed = true;
+                if (p1Script != null) p1Script.moveAllowed = true;
                 break;
-
             case 2:
-                player2.GetComponent<pathFollow>().moveAllowed = true;
+                if (p2Script != null) p2Script.moveAllowed = true;
                 break;
         }
     }
