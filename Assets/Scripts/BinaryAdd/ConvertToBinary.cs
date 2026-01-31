6dc355fc53c3cf3c15e73db0c1fde021bcdc2bab
Joel Solis <joelsolisa117@gmail.com>
2026-01-31 16:48:04 +0000

    Complete the binary convertor .

---
+++ Assets/Scripts/BinaryAdd/ConvertToBinary.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+
+namespace BinaryAdd {
+  class ConvertToBinary {
+    
+    /// <summary>
+    /// Converts a number to binary.
+    /// </summary>
+    /// <returns>A string with the representation of the number in binary</returns>
+    public string ConvertNumToBinary(int number) {
+
+      string binaryNumber = "";
+
+      while (number > 0) {
+        int currentBit = number % 2;
+        number /= 2;
+        binaryNumber += currentBit;
+      }
+
+      char[] binaryNumberArr = binaryNumber.ToCharArray();
+      binaryNumberArr.Reverse();
+      
+      binaryNumber = new string(binaryNumberArr);
+      binaryNumber = AddLeadingZeros(binaryNumber);
+        
+      return binaryNumber;
+    }
+
+    public string AddLeadingZeros(string number) {
+      
+      int missingZeros = 7 - number.Length;
+      string missingZerosString = "";
+
+      for (int i = missingZeros; i > 0; i--) {
+        missingZerosString += "0";
+      }
+      
+      return missingZerosString + number;
+    }
+  }
+}
