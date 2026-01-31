680e9f4295c633e3f110deed845ce9111f575b05
Joel Solis <joelsolisa117@gmail.com>
2026-01-31 12:50:16 +0000

    Add controller scripts

---
+++ Assets/Scripts/DiningPhilosophers/Controller.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controller : MonoBehaviour {
+  
+  [SerializeField]
+  private InputActionReference _playerInputActionReference;
+  public void Awake() {
+  }
+
+  private void OnEnable() {
+    throw new NotImplementedException();
+  }
+  
+  private void OnDisable() {
+    throw new NotImplementedException();
+  }
+
+  private void TakeLeftFork() {
+    
+  }
+  
+  
+}
