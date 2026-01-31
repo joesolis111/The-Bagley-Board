6191bfde5bac6997a967d39593f9f81920a0a55b
larleevee <lanavalentine1889@gmail.com>
2026-01-31 12:50:01 +0000

    scripts

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class gameControl : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/diceRoll.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class diceRoll : MonoBehaviour{
+
+    private Sprite[] diceSides;
+    private SpriteRenderer rend;
+    private int whosTurn = 1;
+    private bool coroutineAllowed = true;
+
+    private void Start() {
+        rend = GetComponent<SpriteRenderer>();
+        //"Resources/board/dice/"
+        diceSides = Resources.LoadAll<Sprite>("board/dice/");
+        
+        if (diceSides.Length > 0)
+            rend.sprite = diceSides[5];
+    }
+
+
+    private void OnMouseDown(){
+        if (!GameControl.gameOver && coroutineAllowed)
+            StartCoroutine("RollTheDice"); 
+    }
+
+    private IEnumerator RollTheDice(){
+        coroutineAllowed = false;
+        int randomDiceSide = 0;
+
+        for (int i = 0; i <= 20; i++){
+            randomDiceSide = Random.Range(0, 6);
+            rend.sprite = diceSides[randomDiceSide];
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        GameControl.diceSideThrown = randomDiceSide + 1;
+        
+        if (whosTurn == 1){
+            GameControl.MovePlayer(1);
+        } else if (whosTurn == -1){
+            GameControl.MovePlayer(2);
+        }
+
+        whosTurn *= -1;
+        coroutineAllowed = true;
+    }
+}
